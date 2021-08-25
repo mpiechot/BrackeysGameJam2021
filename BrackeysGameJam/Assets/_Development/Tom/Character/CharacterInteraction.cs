181181ea151d2a49bb2a1c9afa9dfdf5f03b85f8
Tom <15022636+Thaesch@users.noreply.github.com>
2021-08-25 19:35:23 +0200

    Character Interaction + Player Prefab

--- BrackeysGameJam/Assets/_Development/Tom/Character/CharacterInteraction.cs
+++ BrackeysGameJam/Assets/_Development/Tom/Character/CharacterInteraction.cs
@@ -9,28 +9,47 @@ public class CharacterInteraction : MonoBehaviour
     [SerializeField] private LayerMask selectableMask;
 
     private GameObject selectedField;
-    
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Build();
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            CheerUpUnits();
+        }
+    }
+
     void FixedUpdate()
     {
-        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, selectableDistance,selectableMask))
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, selectableDistance, selectableMask))
         {
             selectedField = hit.transform.gameObject;
             selectedField.GetComponent<TowerBuildPosition>().Select();
         }
-        if(Input.GetButtonDown("Fire1"))
-        {
-            Build();
-        }
+    }
+
+    private void CheerUpUnits()
+    {
+        // For all units in Range, reduce chaos
     }
 
     private void Build()
     {
-        if (true) //Todo if enough coins
+        if (EnoughCoins()) //Todo if enough coins
         {
-            selectedField.GetComponent<TowerBuildPosition>().BuildTower(null); //Todo get selected UI Tower
+
+            selectedField?.GetComponent<TowerBuildPosition>().BuildTower(null); //Todo get selected UI Tower
             
             //Todo reduce coins
         }
     }
-    
+
+    private bool EnoughCoins()
+    {
+        return true; 
+    }
 }
